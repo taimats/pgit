a9bf1a8d829ed44e2f56ed30fb328094bdb134a3
taimats <beo8939@gmail.com>
2025-09-20 03:02:25 +0900

    add: init command

--- cmd/init.go
+++ cmd/init.go
@@ -4,41 +4,27 @@ import (
 	"errors"
 	"log"
 	"os"
-	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
-
-const RootDir = ".pgit"
-const ObjDir = "objects"
 
 var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "starting a pgit project",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		pgitDir := PgitDirPath()
-		if pgitDir != "" {
-			return errors.New("pgit is already initialiezed")
+		objdir, err := AbsObjDirPath()
+		if err != nil {
+			return err
 		}
-		if err := os.MkdirAll(filepath.Join(pgitDir, ObjDir), 0400); err != nil {
+		if _, err := os.Stat(objdir); err == nil {
+			return errors.New("already initialized")
+		}
+		if err := os.MkdirAll(objdir, os.ModeDir); err != nil {
 			return err
 		}
 		log.Println("starting a pgit project!!")
 		return nil
 	},
-}
-
-// The returned path is an absolute one including ".pgit" directory.
-func PgitDirPath() string {
-	cwd, err := os.Getwd()
-	if err != nil {
-		return ""
-	}
-	path := filepath.Join(cwd, RootDir)
-	if _, err := os.Stat(path); err != nil {
-		return ""
-	}
-	return path
 }
 
 func init() {

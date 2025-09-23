0cbc78e1e9c9142e8e78131511f841179c5edb3b
taimats <beo8939@gmail.com>
2025-09-23 15:24:59 +0900

    fix: a massive change for a better feature

--- cmd/writeTree.go
+++ cmd/writeTree.go
@@ -10,6 +10,7 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -17,53 +18,87 @@ import (
 // writeTreeCmd represents the writeTree command
 var writeTreeCmd = &cobra.Command{
 	Use:   "write-tree",
-	Short: "save directories",
+	Short: "turn a directory into a tree object and save it",
 	RunE: func(cmd *cobra.Command, args []string) error {
+		err := CheckPgitInit()
+		if err != nil {
+			return err
+		}
 		cwd, err := os.Getwd()
 		if err != nil {
 			return err
 		}
-		var buf bytes.Buffer
-		err = filepath.WalkDir(cwd, func(path string, d fs.DirEntry, err error) error {
-			if err != nil {
-				return err
-			}
-			if path == cwd {
-				return nil
-			}
-			if d.Name() == PgitDir {
-				return nil
-			}
-			f, err := os.Open(path)
-			if err != nil {
-				return err
-			}
-			defer f.Close()
-			b, err := io.ReadAll(f)
-			if err != nil {
-				return err
-			}
-			oid, err := SaveHashObj(cwd, b)
-			if err != nil {
-				return err
-			}
-			if d.IsDir() {
-				fmt.Fprintf(&buf, "%s %s %s\n", "tree", oid, d.Name())
-			} else {
-				fmt.Fprintf(&buf, "%s %s %s\n", ObjTypeBlob, oid, d.Name())
-			}
-			return nil
-		})
+		oid, err := saveTree(cwd)
 		if err != nil {
-			return err
-		}
-		oid, err := SaveHashObj(cwd, buf.Bytes())
-		if err != nil {
-			return err
+			return fmt.Errorf("failed to write tree: %w", err)
 		}
 		fmt.Printf("saved a tree!!\noid: %s\n", oid)
 		return nil
 	},
+}
+
+func saveTree(rootPath string) (oid string, err error) {
+	objdir, err := AbsObjDirPath()
+	if err != nil {
+		return "", err
+	}
+	oid, err = writeTree(objdir, rootPath)
+	if err != nil {
+		return "", err
+	}
+	return oid, nil
+}
+
+func writeTree(storePath, rootPath string) (oid string, err error) {
+	var buf bytes.Buffer
+	err = filepath.WalkDir(rootPath, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if path == rootPath {
+			return nil
+		}
+		if isExcluded(d.Name()) {
+			return nil
+		}
+		if d.IsDir() && !isExcluded(d.Name()) {
+			oid, err := writeTree(storePath, path)
+			if err != nil {
+				return fmt.Errorf("failed to handle dir tree: %w\n{ path: %s }", err, path)
+			}
+			if _, err := fmt.Fprintf(&buf, "%s %s %s\n", "tree", oid, d.Name()); err != nil {
+				return err
+			}
+			return nil
+		}
+		f, err := os.Open(path)
+		if err != nil {
+			return fmt.Errorf("failed to open a file: %w", err)
+		}
+		defer f.Close()
+		b, err := io.ReadAll(f)
+		if err != nil {
+			return fmt.Errorf("failed to read a file: %w", err)
+		}
+		oid, err := SaveHashObj(storePath, b)
+		if err != nil {
+			return err
+		}
+		fmt.Fprintf(&buf, "%s %s %s\n", ObjTypeBlob, oid, d.Name())
+		return nil
+	})
+	if err != nil {
+		return "", err
+	}
+	oid, err = SaveHashObj(storePath, buf.Bytes())
+	if err != nil {
+		return "", err
+	}
+	return oid, nil
+}
+
+func isExcluded(baseName string) bool {
+	return baseName == PgitDir || baseName == ObjDir || strings.HasPrefix(baseName, ".")
 }
 
 func init() {

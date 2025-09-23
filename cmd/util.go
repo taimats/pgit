c7cee98d8a487ad130743e31e724b8755ea36df3
taimats <beo8939@gmail.com>
2025-09-24 02:07:57 +0900

    fix: more economical

--- cmd/util.go
+++ cmd/util.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"errors"
 	"os"
-	"path/filepath"
 )
 
 const PgitDir = ".pgit"
@@ -11,21 +10,8 @@ const ObjDir = "objects"
 
 var ErrNeedPgitInit = errors.New("need initializing pgit first")
 
-func AbsObjDirPath() (string, error) {
-	name := filepath.Join(PgitDir, ObjDir)
-	path, err := filepath.Abs(name)
-	if err != nil {
-		return "", err
-	}
-	return path, nil
-}
-
 func CheckPgitInit() error {
-	objdir, err := AbsObjDirPath()
-	if err != nil {
-		return err
-	}
-	if _, err := os.Stat(objdir); err != nil {
+	if _, err := os.Stat(PgitDir); err != nil {
 		return ErrNeedPgitInit
 	}
 	return nil

35a172d2a24cbdd6e9b3e0d65617a18bc7726764
taimats <beo8939@gmail.com>
2025-09-28 14:02:55 +0900

    add: helper func

--- cmd/util.go
+++ cmd/util.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"bufio"
+	"bytes"
 	"errors"
 	"fmt"
 	"io"
@@ -32,12 +34,12 @@ func CheckPgitInit() error {
 func ReadAllFileContent(path string) (content []byte, err error) {
 	f, err := os.Open(path)
 	if err != nil {
-		return nil, fmt.Errorf("ReadAllFileContent func error: %w", err)
+		return nil, fmt.Errorf("ReadAllFileContent: %w", err)
 	}
 	defer f.Close()
 	content, err = io.ReadAll(f)
 	if err != nil {
-		return nil, fmt.Errorf("ReadAllFileContent func error: %w", err)
+		return nil, fmt.Errorf("ReadAllFileContent: %w", err)
 	}
 	return content, nil
 }
@@ -52,4 +54,37 @@ func WriteFile(path string, content []byte) error {
 	defer f.Close()
 	f.Write(content)
 	return nil
+}
+
+// examine the content of a file in the format like this:
+// -----------------
+// key  value...
+// tree 01234example
+// blob testtest
+// ref  sample
+// .
+// .
+// -----------------
+// If there exists a given key in the file, then returns the corresponding value.
+func ReadValueFromFile(path string, key []byte) (value []byte, err error) {
+	c, err := ReadAllFileContent(path)
+	if err != nil {
+		return nil, fmt.Errorf("ReadValueFromFile: %w", err)
+	}
+	if len(c) == 0 {
+		return nil, err
+	}
+	sc := bufio.NewScanner(bytes.NewReader(c))
+	sc.Split(bufio.ScanLines)
+	for sc.Scan() {
+		l := sc.Bytes()
+		sep := bytes.Split(l, []byte(" "))
+		if len(sep) < 2 {
+			return nil, fmt.Errorf("ReadValueFromFile: invalid data: got=%s", sep)
+		}
+		if bytes.Equal(sep[0], key) {
+			return sep[1], nil
+		}
+	}
+	return nil, nil
 }

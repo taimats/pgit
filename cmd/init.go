e79942e0eebb13af490bbc7c6ba41eb7a6d24a9c
taimats <beo8939@gmail.com>
2025-10-02 00:09:13 +0900

    fix: refactoring init command with data package

--- cmd/init.go
+++ cmd/init.go
@@ -2,39 +2,44 @@ package cmd
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 
 	"github.com/spf13/cobra"
+	"github.com/taimats/pgit/data"
 )
 
-var RooDir string = "."
+var RootDir string = "."
 
 var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "starting a pgit project",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if len(args) > 0 {
-			RooDir = args[0]
+			RootDir = filepath.Join(RootDir, args[0])
 		}
-		if _, err := os.Stat(filepath.Join(RooDir, PgitDir)); err == nil {
+		if _, err := os.Stat(PgitDir); err == nil {
 			return errors.New("already initialized")
 		}
-		if err := os.MkdirAll(filepath.Join(RooDir, PgitDir, ObjDir), os.ModeDir); err != nil {
+		if err := os.MkdirAll(filepath.Join(RootDir, ObjDir), os.ModeDir); err != nil {
 			return err
 		}
-		if err := os.MkdirAll(filepath.Join(RooDir, PgitDir, RefDir, TagDir), os.ModeDir); err != nil {
+		if err := os.MkdirAll(filepath.Join(RootDir, TagDir), os.ModeDir); err != nil {
 			return err
 		}
-		if err := os.MkdirAll(filepath.Join(RooDir, PgitDir, RefDir, HeadDir), os.ModeDir); err != nil {
+		if err := os.MkdirAll(filepath.Join(RootDir, HeadDir), os.ModeDir); err != nil {
 			return err
 		}
-		f, err := os.Create(filepath.Join(RooDir, PgitDir, RefHEAD))
-		if err != nil {
+		refPath := filepath.Join(RootDir, HeadDir, "master")
+		if err := data.WriteFile(refPath, []byte("")); err != nil {
 			return err
 		}
-		f.Close()
+		content := fmt.Sprintf("ref: %s <- HEAD\n", refPath)
+		if err := data.WriteFile(data.RefHEADPath, []byte(content)); err != nil {
+			return err
+		}
 		log.Println("starting a pgit project!!")
 		return nil
 	},

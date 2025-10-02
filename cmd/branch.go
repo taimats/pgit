f0857c95e6558209becd3c32bde36eee19fcf5ad
taimats <beo8939@gmail.com>
2025-10-02 22:04:15 +0900

    add: prints all branches

--- cmd/branch.go
+++ cmd/branch.go
@@ -5,7 +5,9 @@ package cmd
 
 import (
 	"fmt"
+	"io/fs"
 	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/taimats/pgit/data"
@@ -15,12 +17,29 @@ import (
 var branchCmd = &cobra.Command{
 	Use:   "branch",
 	Short: "attach a name to a commit point that HEAD always refers to",
-	Args:  cobra.MinimumNArgs(1),
+	Args:  cobra.MaximumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		name := args[0]
+		var name string
+		if len(args) == 1 {
+			name = args[0]
+		}
+		if name == "" {
+			current, err := currentBranchName()
+			if err != nil {
+				return fmt.Errorf("internal error: %w", err)
+			}
+			list, err := ListBranches(string(current))
+			if err != nil {
+				return fmt.Errorf("internal error: %w", err)
+			}
+			list[0] = fmt.Sprintf("*%s", list[0])
+			str := strings.Join(list, "\n")
+			fmt.Println(str)
+			return nil
+		}
 		_, err := NewBranch(name)
 		if err != nil {
-			return err
+			return fmt.Errorf("internal error: %w", err)
 		}
 		return nil
 	},
@@ -42,6 +61,64 @@ func NewBranch(name string) (path string, err error) {
 		return "", fmt.Errorf("NewBranch: %w", err)
 	}
 	return path, nil
+}
+
+// a list of all the branches with the current one at the top
+func ListBranches(currentBranch string) ([]string, error) {
+	var fns []string
+	fns = append(fns, currentBranch)
+	err := filepath.WalkDir(data.RefBranchPath, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if path == data.RefBranchPath {
+			return nil
+		}
+		if d.IsDir() {
+			return filepath.SkipDir
+		}
+		if d.Name() == currentBranch {
+			return nil
+		}
+		fns = append(fns, d.Name())
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return fns, nil
+}
+
+// reads the HEAD file and returns a current branch name like this:
+// [ ref: .pgit/ref/heads/{name} ] ===> name
+func currentBranchName() (string, error) {
+	current, err := data.ReadValueFromFile(data.RefHEADPath, []byte("ref:"))
+	if err != nil {
+		return "", fmt.Errorf("internal error: %w", err)
+	}
+	return filepath.Base(string(current)), nil
+}
+
+// returning all file names with any dir names excluded
+func allFileNames(dirPath string) ([]string, error) {
+	var fns []string
+	err := filepath.WalkDir(dirPath, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if path == dirPath {
+			return nil
+		}
+		if d.IsDir() {
+			return filepath.SkipDir
+		}
+		fns = append(fns, d.Name())
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return fns, nil
 }
 
 func init() {
